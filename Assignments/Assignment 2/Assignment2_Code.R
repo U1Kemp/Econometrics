9690948bce9d3d9a074dc6d29f702ecba8960d52
U1Kemp <utpalrajkemprai2001@gmail.com>
2025-02-13 20:00:34 +0530

    added descriptive summary in code

--- Assignments/Assignment 2/Assignment2_Code.R
+++ Assignments/Assignment 2/Assignment2_Code.R
@@ -4,9 +4,16 @@
 #-----------------------------------------------------------------------------
 
 # Load libraries
+# For reading Excel file
 library(readxl)
-#library(AER)
-#library(pastecs)
+# For the ordered probit/logit model
+library(MASS)
+# For the brant test
+library(brant)
+# For the generalized ordered logit
+library(VGAM)
+# For marginal effects
+library(erer)
 #-----------------------------------------------------------------------------
 # Question 1.e
 #-----------------------------------------------------------------------------
@@ -135,37 +142,64 @@ table_cont
 #-----------------------------------------------------------------------------
 # Descriptive Summary of Categorical variables
 #-----------------------------------------------------------------------------
+# past use and male
+#-----------------------------------------------------------------------------
+table_use_and_male <- data.frame(
+  Variable = c("Past Use", "Male"),
+  Counts = c(sum(dummy_copy$q57=='Yes'),sum(dummy_copy$sex=="Male")),
+  Percentage = c(mean(dummy_copy$q57=='Yes')*100, mean(dummy_copy$sex=="Male")*100)
+)
 
-# past use
-sum(dummy_copy$q57=='Yes')
-mean(dummy_copy$q57=='Yes')*100
+table_use_and_male
+#  Variable Counts Percentage
+#1 Past Use    719   48.19035
+#2     Male    791   53.01609
+#-----------------------------------------------------------------------------
+# Education
+#-----------------------------------------------------------------------------
+educ_counts = rep(0,3)
+educ_counts[1] = sum(dummy_copy$educ2 == "Postgraduate Degree" | dummy_copy$educ2 == "Four year college"
+                     | dummy_copy$educ2 == "Some Postgraduate")
+educ_counts[2] = sum(dummy_copy$educ2 == 'Some College' | dummy_copy$educ2 == "Associate Degree")
+educ_counts[3] = sum(dummy_copy$educ2 == 'HS' | dummy_copy$educ2 == 'Less than HS' 
+                     | dummy_copy$educ2 == 'HS Incomplete')
 
-# male
-sum(dummy_copy$sex=="Male")
-mean(dummy_copy$sex=="Male")*100
+educ_per = rep(0,3)
+educ_per[1] = 100*mean(dummy_copy$educ2 == "Postgraduate Degree" | dummy_copy$educ2 == "Four year college"
+                     | dummy_copy$educ2 == "Some Postgraduate")
+educ_per[2] = 100*mean(dummy_copy$educ2 == 'Some College' | dummy_copy$educ2 == "Associate Degree")
+educ_per[3] = 100*mean(dummy_copy$educ2 == 'HS' | dummy_copy$educ2 == 'Less than HS' 
+                     | dummy_copy$educ2 == 'HS Incomplete')
 
-# education
-## below bachelor
-sum(dummy_copy$educ2 == 'Some College' | dummy_copy$educ2 == "Associate Degree")
-mean(dummy_copy$educ2 == 'Some College' | dummy_copy$educ2 == "Associate Degree")*100
+table_educ = data.frame(
+  Category = c("Bachelors & Above", "Below Bachelors", "High School & Below"),
+  Counts = educ_counts,
+  Percentage = educ_per
+)
 
-## high school and below
-sum(dummy_copy$educ2 == 'HS' | dummy_copy$educ2 == 'Less than HS' 
-    | dummy_copy$educ2 == 'HS Incomplete')
-mean(dummy_copy$educ2 == 'HS' | dummy_copy$educ2 == 'Less than HS' 
-    | dummy_copy$educ2 == 'HS Incomplete')*100
+table_educ
+#             Category Counts Percentage
+#1   Bachelors & Above    551   36.93029
+#2     Below Bachelors    434   29.08847
+#3 High School & Below    507   33.98123
 
-## bachelors and above
-sum(dummy_copy$educ2 == "Postgraduate Degree" | dummy_copy$educ2 == "Four year college"
-    | dummy_copy$educ2 == "Some Postgraduate")
-mean(dummy_copy$educ2 == "Postgraduate Degree" | dummy_copy$educ2 == "Four year college"
-    | dummy_copy$educ2 == "Some Postgraduate")*100
-
-# eventually legal
+#-----------------------------------------------------------------------------
+# Eventually legal
+#-----------------------------------------------------------------------------
 sum(dummy_copy$q55 == "Yes, it will")
 mean(dummy_copy$q55 == "Yes, it will")*100
+table_even_legal <- data.frame(
+  Variable = 'Eventually legal',
+  Counts = sum(dummy_copy$q55 == "Yes, it will"),
+  Percentage = mean(dummy_copy$q55 == "Yes, it will")*100
+)
+table_even_legal
+#          Variable Counts Percentage
+#1 Eventually legal   1154   77.34584
 
-# race 
+#-----------------------------------------------------------------------------
+# Race
+#-----------------------------------------------------------------------------
 ## White
 sum(dummy_copy$race3m1 == 'White')
 mean(dummy_copy$race3m1 == "White")*100
@@ -180,65 +214,118 @@ length(dummy_copy$educ2) - sum(dummy_copy$race3m1 == 'White') -
 (1 - mean(dummy_copy$race3m1 == 'White') -
   mean(dummy_copy$race3m1 == "Black or African-American"))*100
 
-# party affiliation
-## Republican
-sum(dummy_copy$party == "Republican")
-mean(dummy_copy$party == "Republican")*100
+table_race = data.frame(
+  Category = c("White","African American","Others"),
+  Counts = c(sum(dummy_copy$race3m1 == 'White'),
+             sum(dummy_copy$race3m1 == "Black or African-American"),
+             sum(dummy_copy$race3m1 != "White" 
+                 & dummy_copy$race3m1 != "Black or African-American")),
+  Percentage = c(100*mean(dummy_copy$race3m1 == 'White'),
+                 100*mean(dummy_copy$race3m1 == "Black or African-American"),
+                 100*mean(dummy_copy$race3m1 != "White" 
+                     & dummy_copy$race3m1 != "Black or African-American"))
+)
+table_race
+#          Category Counts Percentage
+#1            White   1149  77.010724
+#2 African American    202  13.538874
+#3           Others    141   9.450402
 
-## Democrat
-sum(dummy_copy$party == "Democrat")
-mean(dummy_copy$party == "Democrat")*100
+#-----------------------------------------------------------------------------
+# Party Affiliation
+#-----------------------------------------------------------------------------
+table_party = data.frame(
+  Category = c("Republican","Democrat","Independent & Others"),
+  Counts = c(sum(dummy_copy$party == "Republican"),
+             sum(dummy_copy$party == "Democrat"),
+             sum(dummy_copy$party != "Republican" 
+                 & dummy_copy$party != "Democrat")),
+  Percentage = c(100*mean(dummy_copy$party == "Republican"),
+                 100*mean(dummy_copy$party == "Democrat"),
+                 100*mean(dummy_copy$party != "Republican" 
+                     & dummy_copy$party != "Democrat"))
+)
+table_party
 
-## Independent and others
-sum(dummy_copy$party != "Democrat" & dummy_copy$party != "Republican")
-mean(dummy_copy$party != "Democrat" & dummy_copy$party != "Republican")*100
+#              Category Counts Percentage
+#1           Republican    333   22.31903
+#2             Democrat    511   34.24933
+#3 Independent & Others    648   43.43164
 
+#-----------------------------------------------------------------------------
 # Religion
+#-----------------------------------------------------------------------------
 ## Christian
-sum(dummy_copy$relig == "Christian (VOL.)")
-mean(dummy_copy$relig == "Christian (VOL.)")*100
+christian_count = sum(dummy_copy$relig == "Christian (VOL.)")
+christian_per = mean(dummy_copy$relig == "Christian (VOL.)")*100
 
 ## Roman Catholic
-sum(dummy_copy$relig == "Roman Catholic (Catholic)")
-mean(dummy_copy$relig == "Roman Catholic (Catholic)")*100
+rom_cat_count = sum(dummy_copy$relig == "Roman Catholic (Catholic)")
+rom_cat_per = mean(dummy_copy$relig == "Roman Catholic (Catholic)")*100
 
 ## Protestant
-sum(dummy_copy$relig == "Protestant (Baptist, Methodist, Non-denominational, Lutheran, Presbyterian, Pentecostal, Episcopalian, Reformed, etc.)")
-mean(dummy_copy$relig == "Protestant (Baptist, Methodist, Non-denominational, Lutheran, Presbyterian, Pentecostal, Episcopalian, Reformed, etc.)")*100
+pro_count = sum(dummy_copy$relig == "Protestant (Baptist, Methodist, Non-denominational, Lutheran, Presbyterian, Pentecostal, Episcopalian, Reformed, etc.)")
+pro_per = mean(dummy_copy$relig == "Protestant (Baptist, Methodist, Non-denominational, Lutheran, Presbyterian, Pentecostal, Episcopalian, Reformed, etc.)")*100
 
 ## Liberal
-sum(dummy_copy$relig == "Agnostic (not sure if there is a God)"
+liberal_count = sum(dummy_copy$relig == "Agnostic (not sure if there is a God)"
     | dummy_copy$relig == "Nothing in particular" 
     | dummy_copy$relig == "Atheist (do not believe in God)"
     | dummy_copy$relig == "Unitarian (Universalist) (VOL.)")
-mean(dummy_copy$relig == "Agnostic (not sure if there is a God)"
+liberal_per = mean(dummy_copy$relig == "Agnostic (not sure if there is a God)"
     | dummy_copy$relig == "Nothing in particular" 
     | dummy_copy$relig == "Atheist (do not believe in God)"
     | dummy_copy$relig == "Unitarian (Universalist) (VOL.)")*100
 
 ## Conservative
-sum(dummy_copy$relig == "Mormon (Church of Jesus Christ of Latter-day Saints/LDS)"
+con_count = sum(dummy_copy$relig == "Mormon (Church of Jesus Christ of Latter-day Saints/LDS)"
     | dummy_copy$relig == "Jewish (Judaism)"
     | dummy_copy$relig == "Hindu"
     | dummy_copy$relig == "Buddhist"
     | dummy_copy$relig == "Muslim (Islam)"
     | dummy_copy$relig == "Orthodox (Greek, Russian, or some other orthodox church)")
-mean(dummy_copy$relig == "Mormon (Church of Jesus Christ of Latter-day Saints/LDS)"
+con_per = mean(dummy_copy$relig == "Mormon (Church of Jesus Christ of Latter-day Saints/LDS)"
     | dummy_copy$relig == "Jewish (Judaism)"
     | dummy_copy$relig == "Hindu"
     | dummy_copy$relig == "Buddhist"
     | dummy_copy$relig == "Muslim (Islam)"
     | dummy_copy$relig == "Orthodox (Greek, Russian, or some other orthodox church)")*100
 
+table_rel <- data.frame(
+  Category = c("Christain","Roman Catholic","Protestant","Liberal","Conservative"),
+  Counts = c(christian_count, rom_cat_count, pro_count, liberal_count, con_count),
+  Percentage = c(christian_per, rom_cat_per, pro_per, liberal_per, con_per)
+)
+
+table_rel
+#        Category Counts Percentage
+#1      Christain    182  12.198391
+#2 Roman Catholic    290  19.436997
+#3     Protestant    550  36.863271
+#4        Liberal    348  23.324397
+#5   Conservative    122   8.176944
+#-----------------------------------------------------------------------------
 # Public Opinion
-## Medicinal
-sum(dummy_copy$q46 == "medicinal")
-mean(dummy_copy$q46 == "medicinal")*100
+#-----------------------------------------------------------------------------
+table_opinion <- data.frame(
+  Category = c("Medicinal","Not Legal","Personal"),
+  Counts = c(sum(dummy_copy$q46 == "medicinal"),sum(dummy_copy$q46 == "notlegal"),
+             sum(dummy_copy$q46 == "personal")),
+  Percentage = c(100*mean(dummy_copy$q46 == "medicinal"),100*mean(dummy_copy$q46 == "notlegal"),
+                 100*mean(dummy_copy$q46 == "personal"))
+)
+table_opinion
+#   Category Counts Percentage
+#1 Medicinal    640   42.89544
+#2 Not Legal    218   14.61126
+#3  Personal    634   42.49330
+#-----------------------------------------------------------------------------
+# ii) Analyze public opinion on extent marijuana legalization in the US i.e., estimate Model 8
+#-----------------------------------------------------------------------------
 
-## NotLegal
-sum(dummy_copy$q46 == "notlegal")
-mean(dummy_copy$q46 == "notlegal")*100
-
-## Personal use
-sum(dummy_copy$q46 == "personal")
-mean(dummy_copy$q46 == "personal")*100
+OrdLogistic <- polr(factor(q46) ~ log_age + log_income + factor(relig) + q57 + factor(educ2) +
+                      race3m1 + party + hh1 + q55 ,
+                    data = dummy_copy, 
+                    method = 'logistic' # change to 'probit' for ordered probit
+)
+summary(OrdLogistic)
