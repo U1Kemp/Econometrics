9af4be71504cb3e6c74bababfa61208e7d52a238
U1Kemp <utpalrajkemprai2001@gmail.com>
2025-02-13 18:15:30 +0530

    added 1.e in R code

---
+++ Assignments/Assignment 2/Assignment2_Code.R
@@ -0,0 +1,244 @@
+#-----------------------------------------------------------------------------
+# Coding part of Econometrics Homework 2
+# Utpalraj Kemprai
+#-----------------------------------------------------------------------------
+
+# Load libraries
+library(readxl)
+#library(AER)
+#library(pastecs)
+#-----------------------------------------------------------------------------
+# Question 1.e
+#-----------------------------------------------------------------------------
+# read the data from file
+file_path <- "C:/Users/utpal/Documents/Notes/Projects/Econometrics/Assignments/Assignment 2/Feb14Data.xlsx"
+Feb14Data <- read_excel(file_path, 
+                        col_types = c("text", "text", "text", 
+                                      "text", "numeric", "text", "text", 
+                                      "text", "text", "numeric", "text", 
+                                      "text", "skip", "skip"))
+View(Feb14Data)
+#-----------------------------------------------------------------------------
+# check the columns
+#-----------------------------------------------------------------------------
+colnames(Feb14Data)
+
+# [1] "q46"     "q57"     "relig"   "sex"     "age"     "educ2"   "race3m1" "income"  "party"  
+# [10] "hh1"     "state"   "q55" 
+
+#-----------------------------------------------------------------------------
+# Column Description
+#-----------------------------------------------------------------------------
+# q46 -> answer to question about legalization: medicinal, notlegal, personal
+# q57 -> answer to question about past use: No, Yes
+# relig -> religion of respondent: 13 unique values
+# sex -> sex of respondent: Female, Male
+# age -> age of the respondent
+# educ2 -> education level of the respondent
+# race3m1 -> race of the respondent
+# income -> income of the respondent
+# party -> political party affiliation of respondent
+# hh1 -> Household size of the respondent
+# state -> state of the respondent
+# q55 -> answer to question: if marijuana will be legalized
+
+#-----------------------------------------------------------------------------
+# view the unique entries/number of unique entries in a column
+#-----------------------------------------------------------------------------
+for(i in colnames(Feb14Data)){
+  # if less than 7, print all unique entries
+  if(length(unique(Feb14Data[[i]])) <= 8){
+    print(paste("Column",i))
+    cat("Unique entries: ")
+    cat(unique(Feb14Data[[i]]),sep = '; ')
+    cat("",sep = '\n')
+    cat("",sep = '\n')
+  }
+  # else print only the number of unique entries
+  else{
+    print(paste("Column",i))
+    print(paste('Number of unique entries:', length(unique(Feb14Data[[i]]))))
+    cat("",sep = '\n')
+  }
+}
+#-----------------------------------------------------------------------------
+# (i) Descriptive Summary of the data
+#-----------------------------------------------------------------------------
+# Take log of age
+dummy_copy = Feb14Data
+
+dummy_copy$age = log(Feb14Data$age)
+names(dummy_copy)[names(dummy_copy) == "age"] <- "log_age"
+
+# Convert income to numbers and take log
+quantify_income <- function(text){
+  if(text == "50 to under $75,000"){
+    return(62500)
+  }
+  else if(text == "20 to under $30,000"){
+    return(25000)
+  }
+  else if(text == "10 to under $20,000"){
+    return(15000)
+  }
+  else if(text == "30 to under $40,000"){
+    return(35000)
+  }
+  else if(text == "Less than $10,000"){
+    return(5000)
+  }
+  else if(text == "$150,000 or more"){
+    return(150000)
+  }
+  else if(text == "100 to under $150,000"){
+    return(125000)
+  }
+  else if(text == "40 to under $50,000"){
+    return(45000)
+  }
+  else if(text == "75 to under $100,000"){
+    return(87500)
+  }
+}
+
+dummy_copy$income = sapply(dummy_copy$income,quantify_income) 
+dummy_copy$income = log(dummy_copy$income)
+names(dummy_copy)[names(dummy_copy) == "income"] <- "log_income"
+
+dummy_copy
+
+#-----------------------------------------------------------------------------
+# Descriptive Summary of Continuous variables
+#-----------------------------------------------------------------------------
+means <- rep(0,3)
+stdev <- rep(0,3)
+ct = 1
+for(col in c('log_age','log_income','hh1')){
+  means[ct] = mean(dummy_copy[[col]])
+  stdev[ct] = sd(dummy_copy[[col]])
+  ct = ct + 1
+}
+
+# Create a tables for Descriptive Summary of Continuous Variables
+table_cont <- data.frame(
+  Variable = c('log_age','log_income','hh1'),
+  Mean = means,
+  StdDev = stdev
+)
+
+table_cont
+#    Variable      Mean    StdDev
+#1    log_age  3.724690 0.4442719
+#2 log_income 10.628089 0.9812756
+#3        hh1  2.737936 1.4209633
+
+#-----------------------------------------------------------------------------
+# Descriptive Summary of Categorical variables
+#-----------------------------------------------------------------------------
+
+# past use
+sum(dummy_copy$q57=='Yes')
+mean(dummy_copy$q57=='Yes')*100
+
+# male
+sum(dummy_copy$sex=="Male")
+mean(dummy_copy$sex=="Male")*100
+
+# education
+## below bachelor
+sum(dummy_copy$educ2 == 'Some College' | dummy_copy$educ2 == "Associate Degree")
+mean(dummy_copy$educ2 == 'Some College' | dummy_copy$educ2 == "Associate Degree")*100
+
+## high school and below
+sum(dummy_copy$educ2 == 'HS' | dummy_copy$educ2 == 'Less than HS' 
+    | dummy_copy$educ2 == 'HS Incomplete')
+mean(dummy_copy$educ2 == 'HS' | dummy_copy$educ2 == 'Less than HS' 
+    | dummy_copy$educ2 == 'HS Incomplete')*100
+
+## bachelors and above
+sum(dummy_copy$educ2 == "Postgraduate Degree" | dummy_copy$educ2 == "Four year college"
+    | dummy_copy$educ2 == "Some Postgraduate")
+mean(dummy_copy$educ2 == "Postgraduate Degree" | dummy_copy$educ2 == "Four year college"
+    | dummy_copy$educ2 == "Some Postgraduate")*100
+
+# eventually legal
+sum(dummy_copy$q55 == "Yes, it will")
+mean(dummy_copy$q55 == "Yes, it will")*100
+
+# race 
+## White
+sum(dummy_copy$race3m1 == 'White')
+mean(dummy_copy$race3m1 == "White")*100
+
+## African American
+sum(dummy_copy$race3m1 == "Black or African-American")
+mean(dummy_copy$race3m1 == "Black or African-American")*100
+
+## Others
+length(dummy_copy$educ2) - sum(dummy_copy$race3m1 == 'White') -
+  sum(dummy_copy$race3m1 == "Black or African-American")
+(1 - mean(dummy_copy$race3m1 == 'White') -
+  mean(dummy_copy$race3m1 == "Black or African-American"))*100
+
+# party affiliation
+## Republican
+sum(dummy_copy$party == "Republican")
+mean(dummy_copy$party == "Republican")*100
+
+## Democrat
+sum(dummy_copy$party == "Democrat")
+mean(dummy_copy$party == "Democrat")*100
+
+## Independent and others
+sum(dummy_copy$party != "Democrat" & dummy_copy$party != "Republican")
+mean(dummy_copy$party != "Democrat" & dummy_copy$party != "Republican")*100
+
+# Religion
+## Christian
+sum(dummy_copy$relig == "Christian (VOL.)")
+mean(dummy_copy$relig == "Christian (VOL.)")*100
+
+## Roman Catholic
+sum(dummy_copy$relig == "Roman Catholic (Catholic)")
+mean(dummy_copy$relig == "Roman Catholic (Catholic)")*100
+
+## Protestant
+sum(dummy_copy$relig == "Protestant (Baptist, Methodist, Non-denominational, Lutheran, Presbyterian, Pentecostal, Episcopalian, Reformed, etc.)")
+mean(dummy_copy$relig == "Protestant (Baptist, Methodist, Non-denominational, Lutheran, Presbyterian, Pentecostal, Episcopalian, Reformed, etc.)")*100
+
+## Liberal
+sum(dummy_copy$relig == "Agnostic (not sure if there is a God)"
+    | dummy_copy$relig == "Nothing in particular" 
+    | dummy_copy$relig == "Atheist (do not believe in God)"
+    | dummy_copy$relig == "Unitarian (Universalist) (VOL.)")
+mean(dummy_copy$relig == "Agnostic (not sure if there is a God)"
+    | dummy_copy$relig == "Nothing in particular" 
+    | dummy_copy$relig == "Atheist (do not believe in God)"
+    | dummy_copy$relig == "Unitarian (Universalist) (VOL.)")*100
+
+## Conservative
+sum(dummy_copy$relig == "Mormon (Church of Jesus Christ of Latter-day Saints/LDS)"
+    | dummy_copy$relig == "Jewish (Judaism)"
+    | dummy_copy$relig == "Hindu"
+    | dummy_copy$relig == "Buddhist"
+    | dummy_copy$relig == "Muslim (Islam)"
+    | dummy_copy$relig == "Orthodox (Greek, Russian, or some other orthodox church)")
+mean(dummy_copy$relig == "Mormon (Church of Jesus Christ of Latter-day Saints/LDS)"
+    | dummy_copy$relig == "Jewish (Judaism)"
+    | dummy_copy$relig == "Hindu"
+    | dummy_copy$relig == "Buddhist"
+    | dummy_copy$relig == "Muslim (Islam)"
+    | dummy_copy$relig == "Orthodox (Greek, Russian, or some other orthodox church)")*100
+
+# Public Opinion
+## Medicinal
+sum(dummy_copy$q46 == "medicinal")
+mean(dummy_copy$q46 == "medicinal")*100
+
+## NotLegal
+sum(dummy_copy$q46 == "notlegal")
+mean(dummy_copy$q46 == "notlegal")*100
+
+## Personal use
+sum(dummy_copy$q46 == "personal")
+mean(dummy_copy$q46 == "personal")*100
